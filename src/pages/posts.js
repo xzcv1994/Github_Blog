b01fa63cb57b041f16637ac474bb036b275b4d4d
yangseungbin <seungbin.yang@kt.com>
2021-10-10 15:37:46 +0900

    router 적용 테스트1

---
+++ src/pages/posts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
